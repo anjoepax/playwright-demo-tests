5914eaed2fa2d68bd634c477bfcd76d8dadeb29d
amanigos <amanigos@thecodeguardian.com>
2024-04-26 15:52:14 +0800

    AJM: Added place order test scenario 2

--- src/tests/sanity/placeorder.spec.ts
+++ src/tests/sanity/placeorder.spec.ts
@@ -2,6 +2,7 @@ import CartPage from "@src/pages/cart.page";
 import {getCustomerData} from "@src/data/random.data";
 import {expect, test} from "@playwright/test";
 import StorePage from "@src/pages/store.page";
+import ProductPage from "@src/pages/product.page";
 
 test.describe("Checkout tests", {tag: ["@sanityTests", "@checkoutTest"]}, () => {
     test("guest checkout using direct bank transfer",
@@ -16,6 +17,28 @@ test.describe("Checkout tests", {tag: ["@sanityTests", "@checkoutTest"]}, () => 
             const checkoutPage = await cartPage.goToCheckoutPage();
             await checkoutPage.open();
             await checkoutPage.fillUpBillingDetails(getCustomerData());
+            await checkoutPage.clickPlaceOrderButton();
+            await expect(checkoutPage.orderSuccessMsg).toHaveText(
+                'Thank you. Your order has been received.'
+            );
+        });
+
+    test("login and checkout using direct bank transfer",
+        async ({page}) => {
+            const storePage = new StorePage(page);
+            await storePage.open();
+            await storePage.searchForProduct('Blue Shoes');
+
+            const productPage = new ProductPage(page);
+            await productPage.clickAddToCartButton();
+
+            const cartPage = new CartPage(page);
+            await cartPage.open();
+            const checkoutPage = await cartPage.goToCheckoutPage();
+            await checkoutPage.open();
+
+            //Login before placing order
+            await checkoutPage.login();
             await checkoutPage.clickPlaceOrderButton();
             await expect(checkoutPage.orderSuccessMsg).toHaveText(
                 'Thank you. Your order has been received.'
